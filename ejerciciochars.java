762affb2c93fbeebc59154b58fe40de36ad7c2e5
nickolasnolte1 <61555652+nickolasnolte1@users.noreply.github.com>
2021-02-15 17:32:01 -0600

    Add files via upload

---
+++ ejerciciochars.java
@@ -0,0 +1,46 @@
+import java.util.Scanner;
+
+class ejerciciochars {
+    static final int MAX_CHAR = 512;
+ 
+    static void getchar(String str)
+    {
+         
+        int count = 0;
+        System.out.println("Enter a sentence :");
+        Scanner sc = new Scanner(System.in);
+        
+ 
+        int len = str.length();
+ 
+        
+        for (int i = 0; i < len; i++)
+            count[str.charAt(i)]++;
+ 
+        
+        char ch[] = new char[str.length()];
+        for (int i = 0; i < len; i++) {
+            ch[i] = str.charAt(i);
+            int find = 0;
+            for (int j = 0; j <= i; j++) {
+ 
+                
+                if (str.charAt(i) == ch[j])
+                    find++;
+            }
+ 
+            if (find == 1)
+                System.out.println(
+                    "La letra "
+                    + str.charAt(i)
+                    + " aparece: " + count[str.charAt(i)] + " veces");
+        }
+    }
+   
+  
+    public static void main(String[] args)
+    {
+        String str = "ejercicio chars";
+        getchar(str);
+    }
+}
